d6983a703f5479bd760acfcea524601154802bfe
pk1260 <18251@ma-web.nl>
2019-10-29 11:44:27 +0100

    Update

--- intersectionObserver.js
+++ intersectionObserver.js
@@ -1,21 +1,32 @@
 let alleLinks = document.querySelectorAll('nav a');
 let deSecties = document.querySelectorAll('section');
 
-const opties = {};
+const opties = {
+    // rootMargin: '-150px',
+    treshold: 1.0
+};
 
 const verwerkDoorsnijding = (entries, observer) => {
-    entries.forEach(entry => {
-        console.log(entries.target + " Doorsnijdt " + entry.isIntersecting);
-    })
+    entries.forEach( entry => {
+        console.log(entry.target.parentNode.id + " Doorsnijdt " + entry.isIntersecting);
+        if (entry.isIntersecting) {
+            let link = zoekBijpassendeLink('#' + entry.target.parentNode.id);
+            maakActief(link);
+        }
+    });
 }
 
-let observer = new IntersectionObserver(verwerkDoorsnijding, opties)
+let observer = new IntersectionObserver(verwerkDoorsnijding, opties);
 
-observer.observe(deSecties[1]);
+// observer.observe(deSecties[1]);
+
+deSecties.forEach( sectie => {
+    observer.observe(sectie.getElementsByTagName('p')[0]);
+})
 
 // functies die de class actief verwijderen uit menu
 const verwijderActief = () => {
-    alleLinks.forEach( (link) =>{
+    alleLinks.forEach((link) => {
         if (link.classList = 'actief') {
             link.classList.remove('actief')
         }
@@ -28,10 +39,15 @@ const maakActief = (elem) => {
     elem.classList.add('actief');
 }
 
-alleLinks.forEach( (link) => {
+alleLinks.forEach((link) => {
     link.addEventListener('click', (e) => {
         e.preventDefault();
         maakActief(e.target);
         window.location = e.target.href;
     })
 })
+
+const zoekBijpassendeLink = (id) => {
+    let link = document.querySelector('nav a[href="' + id + '"]');
+    return link;
+}
